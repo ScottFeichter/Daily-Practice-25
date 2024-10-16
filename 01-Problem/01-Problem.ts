c2df2fce1653bcf71716cb648e1d20519b944938
Scott Feichter <scottfeichter@gmail.com>
2024-10-15 20:26:06 -0400

    misc (msg via sh alias)

--- 01-Problem/01-Problem.ts
+++ 01-Problem/01-Problem.ts
@@ -1,5 +1,7 @@
 // Make two immutable integer variables and add them together in a third variable
-var aa = 8;
-var bb = 16;
-var cc = aa + bb;
+
+let aa: number = 8;
+let bb: number = 16;
+let cc: number = aa + bb;
+
 console.log(cc);
