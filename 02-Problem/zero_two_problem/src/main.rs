6680b63d057eecdf9a0400165d3fbb536c8452ea
Scott Feichter <scottfeichter@gmail.com>
2024-10-21 09:14:59 -0400

    misc (msg via sh alias)

--- 02-Problem/zero_two_problem/src/main.rs
+++ 02-Problem/zero_two_problem/src/main.rs
@@ -1,5 +1,13 @@
 fn main() {
-   
+    // cargo new the_name
+    // cargo run
+
     // create a function that takes two numbers input and outputs their product
+
+    fn multiply(num1, num2) {
+        return num1 * num2;
+    }
+
+    println!(multiply(3,4));
 
 }
---
+++ 02-Problem/zero_two_problem/target/debug/deps/zero_two_problem-892292cc6c3de09f.d
@@ -0,0 +1,5 @@
+/Users/scottfeichter/Dropbox/____DEV 2025/Daily-Practice-25/02-Problem/zero_two_problem/target/debug/deps/zero_two_problem-892292cc6c3de09f: src/main.rs
+
+/Users/scottfeichter/Dropbox/____DEV 2025/Daily-Practice-25/02-Problem/zero_two_problem/target/debug/deps/zero_two_problem-892292cc6c3de09f.d: src/main.rs
+
+src/main.rs:
