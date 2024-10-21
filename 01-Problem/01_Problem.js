55e3e0a7f3eac48221d5e02ad706380ec5a89e3c
Scott Feichter <scottfeichter@gmail.com>
2024-10-21 09:18:04 -0400

    misc (msg via sh alias)

--- 01-Problem/01_Problem.js
+++ 01-Problem/01_Problem.js
@@ -1,5 +1,6 @@
+"use strict";
 // Make two immutable integer variables and add them together in a third variable
-var aa = 8;
-var bb = 16;
-var cc = aa + bb;
+let aa = 8;
+let bb = 16;
+let cc = aa + bb;
 console.log(cc);
--- 02-Problem/02-Problem-TS/02-Problem.js
+++ 02-Problem/02-Problem-TS/02-Problem.js
@@ -1,9 +1,6 @@
+"use strict";
 // create a function that takes two numbers input and outputs their product
-
-
-const multiply = (num1: number, num2: number) => {
-  return num1 * num2
-}
-
-
+const multiply = (num1, num2) => {
+    return num1 * num2;
+};
 console.log(multiply(3, 4));
