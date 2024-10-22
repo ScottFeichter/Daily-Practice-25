baac2315b8e19d7c7bbc8aaa65b4e9b2145f27b7
Scott Feichter <scottfeichter@gmail.com>
2024-10-22 14:51:59 -0400

    misc (msg via sh alias)

---
+++ 03-Problem-If/03_Problem.py
@@ -0,0 +1,2 @@
+# create a function with an if statement that returns true if an integer is even or false if it is odd
+# print examples with the result for both outcomes to the log
---
+++ 03-Problem-If/zero_three_problem/src/main.rs
@@ -0,0 +1,6 @@
+// create a function with an if statement that returns true if an integer is even or false if it is odd
+// print examples with the result for both outcomes to the log
+
+fn main() {
+    println!("Hello, world!");
+}
