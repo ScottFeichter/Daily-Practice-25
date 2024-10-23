4b14cfa2ec827da2bc526d3fa1d423fb9fce51ce
Scott Feichter <scottfeichter@gmail.com>
2024-10-22 20:20:38 -0400

    misc (msg via sh alias)

---
+++ 03-Problem-If/03_Problem_Java/ZeroThreeProblem.java
@@ -0,0 +1,20 @@
+// create a function with an if statement that returns true if an integer is even or false if it is odd
+// print examples with the result for both outcomes to the log
+
+public class ZeroThreeProblem {
+
+    static boolean isEven(Integer num1) {
+        if (num1 % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main (String[] args) {
+
+        System.out.println(isEven(46));
+        System.out.println(isEven(93));
+    }
+
+
+}
