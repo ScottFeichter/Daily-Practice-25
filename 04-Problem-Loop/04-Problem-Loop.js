31e327ee643d973ea72b1316f861f3e0be8bf674
Scott Feichter <scottfeichter@gmail.com>
2024-10-24 12:10:56 -0700

    misc (msg via sh alias)

---
+++ 04-Problem-Loop/04-Problem-Loop.js
@@ -0,0 +1,9 @@
+// create an immutable variable sum = 1 and print it then create a basic for loop and increment sum 9 times to total 10 then print sum
+let sum = 1;
+console.log("sum: ", sum);
+
+for (let i = 0; i < 9; i++) {
+   sum++
+}
+
+console.log("sum: ", sum);
--- dailyLang.zsh
+++ dailyLang.zsh
@@ -10,18 +10,60 @@
 problem_number=$1
 problem_description=$2
 
+folder_name="$problem_number-Problem-$problem_description"
 
-folder_name="$problem_number'-'Problem'-'$problem_description"
+
+
+nums=(Zero One Two Three Four Five Six Seven Eight Nine)
+
+first_num=$((${problem_number[1]} + 1))
+second_num=$((${problem_number[2]} + 1))
+problem_num_name="$nums[$first_num]:l_$nums[$second_num]:l_problem_$problem_description:l"
+
+java_prob_num_name="$nums[$first_num]$nums[$second_num]Problem$problem_description"
 
 mkdir $folder_name
 cd $folder_name
 
-ts="$problem_number'-'Problem'-'$problem_description'-'TS"
-mkdir ts
+
+zsh="$problem_number-Problem-$problem_description.zsh"
+touch $zsh
+
+js="$problem_number-Problem-$problem_description.js"
+touch $js
+
+py="${problem_number}_Problem_$problem_description.py"
+touch $py
+
+swift="$problem_number-Problem-$problem_description.swift"
+touch $swift
+
+ts="$problem_number-Problem-$problem_description-TS"
+mkdir $ts
+cd $ts
+touch "$problem_number-Problem-$problem_description.ts"
+cd ..
 
 
-#js
-#java = $folder_name + "
-#cpp
-#r
-#py
+
+cpp="$problem_number-Problem-$problem_description-CPP"
+mkdir $cpp
+cd $cpp
+touch "$problem_number-Problem-$problem_description.cpp"
+cd ..
+
+
+java="${problem_number}_Problem_${problem_description}_JAVA"
+mkdir $java
+cd $java
+touch "$java_prob_num_name.java"
+cd ..
+
+
+r="${problem_num_name}_rust"
+cargo new $r
+
+
+
+gaacp
+clear
