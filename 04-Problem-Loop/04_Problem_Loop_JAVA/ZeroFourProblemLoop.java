1703035d67a68df3706bac5d17e1f8527b70444a
Scott Feichter <scottfeichter@gmail.com>
2024-10-30 12:52:56 -0700

    misc (msg via sh alias)

--- 04-Problem-Loop/04_Problem_Loop_JAVA/ZeroFourProblemLoop.java
+++ 04-Problem-Loop/04_Problem_Loop_JAVA/ZeroFourProblemLoop.java
@@ -1,1 +1,18 @@
 // create a mutable variable sum = 1 and print it then create a basic for loop and increment sum 9 times to total 10 then print sum
+
+public class ZeroFourProblemLoop {
+
+    public static void main (String[]  args) {
+        int sum = 1;
+        System.out.println(sum);
+
+        for(int i = 0; i < 9; i++) { // a for loop is slightly redundant - while loop would suffice
+            sum++;
+        }
+
+        System.out.println(sum);
+
+
+    }
+
+}
