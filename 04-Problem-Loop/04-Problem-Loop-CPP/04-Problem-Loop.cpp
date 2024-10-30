9b2c917ea8d1937488e4dae4d7517560243df9c3
Scott Feichter <scottfeichter@gmail.com>
2024-10-30 13:22:32 -0700

    misc (msg via sh alias)

--- 04-Problem-Loop/04-Problem-Loop-CPP/04-Problem-Loop.cpp
+++ 04-Problem-Loop/04-Problem-Loop-CPP/04-Problem-Loop.cpp
@@ -1,1 +1,19 @@
 // create a mutable variable sum = 1 and print it then create a basic for loop and increment sum 9 times to total 10 then print sum
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int sum = 1;
+  cout << sum << endl;
+
+  while (sum < 10) {
+    sum++;
+  }
+
+  cout << sum << endl;
+
+
+ return 0;
+}
