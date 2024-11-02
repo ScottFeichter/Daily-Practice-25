d5762b012cfa4a9ded9dc3deb2ed3a095a7cb37b
Scott Feichter <scottfeichter@gmail.com>
2024-11-01 19:40:43 -0700

    misc (msg via sh alias)

--- 04-Problem-Loop/04-Problem-Loop-TS/04-Problem-Loop.js
+++ 04-Problem-Loop/04-Problem-Loop-TS/04-Problem-Loop.js
@@ -1,11 +1,7 @@
 // create a mutable variable sum = 1 and print it then create a basic for loop and increment sum 9 times to total 10 then print sum
-
-let sum = 1;
-
+var sum = 1;
 console.log(sum);
-
 while (sum < 10) {
-  sum++
+    sum++;
 }
-
 console.log(sum);
