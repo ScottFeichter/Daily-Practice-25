84d2be16d62a38415ac1a3fb965c890e3ab6842c
Scott Feichter <scottfeichter@gmail.com>
2024-11-03 23:09:26 -0800

    misc (msg via sh alias)

--- 05-Problem-Variables/05_Problem_Variables_JAVA/ZeroFiveProblemVariables.java
+++ 05-Problem-Variables/05_Problem_Variables_JAVA/ZeroFiveProblemVariables.java
@@ -1,1 +1,16 @@
 // Create a mutable variable and an immutable variable and print each in one log.  Add the immutable variable to the mutable variable. Again print both in a new log.
+
+public class ZeroFiveProblemVariables {
+  public static void main(String[] args) {
+    int mutable = 5;
+    final int immutable = 6;
+
+    System.out.println("mutable: " + mutable + " immutable: " + immutable);
+
+    mutable += immutable;
+
+    System.out.println("mutable: " + mutable + " immutable: " + immutable);
+
+  }
+
+}
