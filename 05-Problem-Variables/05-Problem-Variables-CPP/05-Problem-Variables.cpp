82041022e3fd32e9a013fe7d28b5e9f57438c301
Scott Feichter <scottfeichter@gmail.com>
2024-11-03 22:58:49 -0800

    misc (msg via sh alias)

--- 05-Problem-Variables/05-Problem-Variables-CPP/05-Problem-Variables.cpp
+++ 05-Problem-Variables/05-Problem-Variables-CPP/05-Problem-Variables.cpp
@@ -1,1 +1,18 @@
 // Create a mutable variable and an immutable variable and print each in one log.  Add the immutable variable to the mutable variable. Again print both in a new log.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int myMutable = 5;
+    const int immutable = 6;
+
+    cout << "mutable: " << myMutable << " immutable: " << immutable << endl;
+
+    myMutable += immutable;
+
+    cout << "mutable: " << myMutable << " immutable: " << immutable << endl;
+
+  return 0;
+}
