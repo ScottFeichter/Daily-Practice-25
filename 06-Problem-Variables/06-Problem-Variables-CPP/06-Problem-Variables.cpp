1519bff1c9b7d6a2594e4e39f2cdf2f9d9306f43
Scott Feichter <scottfeichter@gmail.com>
2024-11-04 12:08:12 -0800

    misc (msg via sh alias)

--- 06-Problem-Variables/06-Problem-Variables-CPP/06-Problem-Variables.cpp
+++ 06-Problem-Variables/06-Problem-Variables-CPP/06-Problem-Variables.cpp
@@ -1,1 +1,18 @@
 // Create a mutable variable and an immutable variable and print each in one log.  Add the immutable variable to the mutable variable. Again print both in a new log.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int myMutable = 4;
+    const int MY_IMMUTABLE = 5;
+
+    cout << "myMutable: " << myMutable << " MY_IMMUTABLE: " << MY_IMMUTABLE << endl;
+
+    myMutable += MY_IMMUTABLE;
+
+    cout << "myMutable: " << myMutable << " MY_IMMUTABLE: " << MY_IMMUTABLE << endl;
+
+    return 0;
+}
--- 06-Problem-Variables/06_Problem_Variables_JAVA/ZeroSixProblemVariables.java
+++ 06-Problem-Variables/06_Problem_Variables_JAVA/ZeroSixProblemVariables.java
@@ -1,1 +1,19 @@
 // Create a mutable variable and an immutable variable and print each in one log.  Add the immutable variable to the mutable variable. Again print both in a new log.
+
+
+public class ZeroSixProblemVariables {
+
+
+    public static void main(String[] args){
+
+        int myMutable = 4;
+        final int MY_IMMUTABLE = 5;
+
+        System.out.println("myMutable: " + myMutable + " MY_IMMUTABLE: " + MY_IMMUTABLE);
+
+        myMutable += MY_IMMUTABLE;
+
+        System.out.println("myMutable: " + myMutable + " MY_IMMUTABLE: " + MY_IMMUTABLE);
+
+    }
+}
