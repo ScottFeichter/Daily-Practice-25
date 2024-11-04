337ddf84ed67009a55868ea7ea40fab0ffc5959c
Scott Feichter <scottfeichter@gmail.com>
2024-11-03 22:42:31 -0800

    misc (msg via sh alias)

--- 05-Problem-Variables/05-Problem-Variables-TS/05-Problem-Variables.js
+++ 05-Problem-Variables/05-Problem-Variables-TS/05-Problem-Variables.js
@@ -1,10 +1,6 @@
 // Create a mutable variable and an immutable variable and print each in one log.  Add the immutable variable to the mutable variable. Again print both in a new log.
-
-let mutable: number = 5;
-const IMMUTABLE: number = 6;
-
-console.log(`mutable: ${mutable} IMMUTABLE: ${IMMUTABLE}`);
-
+var mutable = 5;
+var IMMUTABLE = 6;
+console.log("mutable: ".concat(mutable, " IMMUTABLE: ").concat(IMMUTABLE));
 mutable += IMMUTABLE;
-
-console.log(`mutable: ${mutable} IMMUTABLE: ${IMMUTABLE}`);
+console.log("mutable: ".concat(mutable, " IMMUTABLE: ").concat(IMMUTABLE));
