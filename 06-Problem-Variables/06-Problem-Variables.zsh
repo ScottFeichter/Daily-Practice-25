7a462f8cdf988ffe46a21774e8ee664a8d3d18f2
Scott Feichter <scottfeichter@gmail.com>
2024-11-04 11:53:48 -0800

    misc (msg via sh alias)

--- 06-Problem-Variables/06-Problem-Variables.zsh
+++ 06-Problem-Variables/06-Problem-Variables.zsh
@@ -1,1 +1,10 @@
 # Create a mutable variable and an immutable variable and print each in one log.  Add the immutable variable to the mutable variable. Again print both in a new log.
+
+myMutable=4;
+readonly MY_IMMUTABLE=5;
+
+echo "myMutable: " $myMutable " MY_IMMUTABLE: " $MY_IMMUTABLE
+
+((myMutable+=$MY_IMMUTABLE))
+
+echo "myMutable: " $myMutable " MY_IMMUTABLE: " $MY_IMMUTABLE
