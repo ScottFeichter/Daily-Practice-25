470f4337a94ed6375aa020ac89a9807cf9d1d513
Scott Feichter <scottfeichter@gmail.com>
2024-11-04 11:45:01 -0800

    misc (msg via sh alias)

--- 06-Problem-Variables/06-Problem-Variables.js
+++ 06-Problem-Variables/06-Problem-Variables.js
@@ -1,10 +1,12 @@
 // Create a mutable variable and an immutable variable and print each in one log.  Add the immutable variable to the mutable variable. Again print both in a new log.
 
 let myMutable = 4;
-const MY_MUTABLE = 5;
+const MY_IMMUTABLE = 5;
 
-console.log("myMutable: ", myMutable, "MY_MUTABLE: ", MY_MUTABLE);
+console.log("myMutable: ", myMutable, "MY_IMMUTABLE: ", MY_IMMUTABLE);
 
-myMutable += MY_MUTABLE;
+myMutable += MY_IMMUTABLE;
 
-console.log("myMutable: ", myMutable, "MY_MUTABLE: ", MY_MUTABLE);
+console.log("myMutable: ", myMutable, "MY_IMMUTABLE: ", MY_IMMUTABLE);
+
+
--- 06-Problem-Variables/06_Problem_Variables.py
+++ 06-Problem-Variables/06_Problem_Variables.py
@@ -1,1 +1,10 @@
 # Create a mutable variable and an immutable variable and print each in one log.  Add the immutable variable to the mutable variable. Again print both in a new log.
+
+myMutable = 4
+MY_IMMUTABLE = 5
+
+print(f"myMutable: {myMutable} MY_IMMUTABLE: {MY_IMMUTABLE}")
+
+myMutable += MY_IMMUTABLE
+
+print(f"myMutable: {myMutable} MY_IMMUTABLE: {MY_IMMUTABLE}")
