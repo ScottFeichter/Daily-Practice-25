6752bc63910466b9349b18da52a4b0190dde2b13
Scott Feichter <scottfeichter@gmail.com>
2024-11-04 11:50:43 -0800

    misc (msg via sh alias)

--- 06-Problem-Variables/06-Problem-Variables.swift
+++ 06-Problem-Variables/06-Problem-Variables.swift
@@ -1,1 +1,10 @@
 // Create a mutable variable and an immutable variable and print each in one log.  Add the immutable variable to the mutable variable. Again print both in a new log.
+
+var myMutable = 4;
+let MY_IMMUTABLE = 5;
+
+print("myMutable: \(myMutable) myMutable MY_IMMUTABLE: \(MY_IMMUTABLE)");
+
+myMutable += MY_IMMUTABLE;
+
+print("myMutable: \(myMutable) myMutable MY_IMMUTABLE: \(MY_IMMUTABLE)");
