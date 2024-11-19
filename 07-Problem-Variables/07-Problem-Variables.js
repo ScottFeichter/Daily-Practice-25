4a71381df4ecab3e1a12d480adc3db5c2a8c57a7
Scott Feichter <scottfeichter@gmail.com>
2024-11-19 00:36:23 -0500

    misc (msg via sh alias)

--- 07-Problem-Variables/07-Problem-Variables.js
+++ 07-Problem-Variables/07-Problem-Variables.js
@@ -1,1 +1,10 @@
 // Create a mutable variable and an immutable variable and print each in one log.  Add the immutable variable to the mutable variable. Again print both in a new log.
+
+let mutable = 10;
+const immutable = 19;
+
+console.log(`mutable: ${mutable} immutable: ${immutable}`);
+
+mutable += immutable;
+
+console.log(`mutable: ${mutable} immutable: ${immutable}`);
