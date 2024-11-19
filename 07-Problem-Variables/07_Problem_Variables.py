bf45ad4988a49f4019d999f1af4792fb5a5baba0
Scott Feichter <scottfeichter@gmail.com>
2024-11-19 00:43:39 -0500

    misc (msg via sh alias)

--- 07-Problem-Variables/07_Problem_Variables.py
+++ 07-Problem-Variables/07_Problem_Variables.py
@@ -1,1 +1,12 @@
 # Create a mutable variable and an immutable variable and print each in one log.  Add the immutable variable to the mutable variable. Again print both in a new log.
+
+my_mutable = 10
+MY_IMMUTABLE = 19
+
+print(f"my_mutable: {my_mutable} my_immutable: {MY_IMMUTABLE}")
+
+my_mutable += MY_IMMUTABLE
+
+print(f"my_mutable: {my_mutable} my_immutable: {MY_IMMUTABLE}")
+
+
