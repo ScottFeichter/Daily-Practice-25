c9de737ecbce7c1587adc8f980ff38bc64f9c397
Scott Feichter <scottfeichter@gmail.com>
2024-11-20 16:43:54 -0500

    misc (msg via sh alias)

--- 08-Problem-Function/08-Problem-Function.zsh
+++ 08-Problem-Function/08-Problem-Function.zsh
@@ -1,2 +1,13 @@
 # create a function that takes two numbers input and outputs their product
 
+function multiply() {
+    return $(($1 * $2))
+}
+
+multiply 5 5
+echo $?
+
+# NOTE: YOU CANNOT RETURN HIGHER THAN 256 BECAUSE THE RETURN IS ACTUALLY A STATUS
+# HOWEVER: YOU CAN JUST DO THE MATH IN ECHO
+
+echo $((5 * 5))
