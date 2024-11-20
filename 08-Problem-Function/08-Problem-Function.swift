75d1417f2789ac251af2dc6dacb51cbf5a576096
Scott Feichter <scottfeichter@gmail.com>
2024-11-20 00:05:12 -0500

    misc (msg via sh alias)

--- 08-Problem-Function/08-Problem-Function.swift
+++ 08-Problem-Function/08-Problem-Function.swift
@@ -1,1 +1,7 @@
 // create a function that takes two numbers input and outputs their product
+
+func multiply(num1: Int, num2: Int) -> Int {
+    return num1 * num2;
+}
+
+print(multiply(num1: 5, num2: 5))
