29d2cb26f8ce318d89c012bdd9d964f68e400fe8
Scott Feichter <scottfeichter@gmail.com>
2024-11-21 00:21:50 -0500

    misc (msg via sh alias)

--- 09-Problem-Function-Multiply-Immut-Mut/09-Problem-Function-Multiply-Immut-Mut.js
+++ 09-Problem-Function-Multiply-Immut-Mut/09-Problem-Function-Multiply-Immut-Mut.js
@@ -1,1 +1,12 @@
 // create a mutable variable and an immutable variable. create a function multiply that returns the product of 2 arguments. print the values of the variables and print their product using the multiply function.
+
+let myMut = 7;
+const myImmut = 9;
+
+const multiply = (num1, num2) => {
+  return num1 * num2;
+}
+
+console.log(`myMut: ${myMut} myImmut: ${myImmut} product: ${multiply(myMut, myImmut)}`);
+
+
