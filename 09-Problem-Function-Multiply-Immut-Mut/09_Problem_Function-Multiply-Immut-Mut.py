812a930c2d1ecc58497c9bcb79a53253d0fef627
Scott Feichter <scottfeichter@gmail.com>
2024-11-21 00:24:21 -0500

    misc (msg via sh alias)

--- 09-Problem-Function-Multiply-Immut-Mut/09_Problem_Function-Multiply-Immut-Mut.py
+++ 09-Problem-Function-Multiply-Immut-Mut/09_Problem_Function-Multiply-Immut-Mut.py
@@ -1,1 +1,11 @@
 # create a mutable variable and an immutable variable. create a function multiply that returns the product of 2 arguments. print the values of the variables and print their product using the multiply function.
+
+myMut = 7
+MY_IMMUT = 9
+
+def multiply(num1, num2):
+    return num1 * num2
+
+print(f"myMut: {myMut} MY_IMMUT: {MY_IMMUT} product: {multiply(myMut, MY_IMMUT)}")
+
+
