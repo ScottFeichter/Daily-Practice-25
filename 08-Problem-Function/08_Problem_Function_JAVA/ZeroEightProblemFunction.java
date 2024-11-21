8bfb09b37e4524be23be717e8b7db2286ce35d8d
Scott Feichter <scottfeichter@gmail.com>
2024-11-20 20:25:43 -0500

    misc (msg via sh alias)

--- 08-Problem-Function/08_Problem_Function_JAVA/ZeroEightProblemFunction.java
+++ 08-Problem-Function/08_Problem_Function_JAVA/ZeroEightProblemFunction.java
@@ -1,3 +1,18 @@
 // create a function that takes two numbers input and outputs their product
 
 
+public class ZeroEightProblemFunction {
+
+    static Integer multiply(Integer num1, Integer num2) {
+        return num1 * num2;
+    }
+
+    public static void main(String[] args) {
+
+
+
+        System.out.println(multiply(5, 5));
+    }
+
+
+}
