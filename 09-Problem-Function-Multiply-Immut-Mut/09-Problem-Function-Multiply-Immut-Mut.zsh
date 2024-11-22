4e7559fe82dc0affb614405c9c01028e208d49e4
Scott Feichter <scottfeichter@gmail.com>
2024-11-22 00:37:00 -0500

    misc (msg via sh alias)

--- 09-Problem-Function-Multiply-Immut-Mut/09-Problem-Function-Multiply-Immut-Mut.zsh
+++ 09-Problem-Function-Multiply-Immut-Mut/09-Problem-Function-Multiply-Immut-Mut.zsh
@@ -1,1 +1,17 @@
 # create a mutable variable and an immutable variable. create a function multiply that returns the product of 2 arguments. print the values of the variables and print their product using the multiply function.
+
+
+myMut=7;
+readonly myImmut=9;
+
+function multiply() {
+    return $(($1 * $2))
+}
+
+multiply $myMut $myImmut
+echo $?
+
+# NOTE: YOU CANNOT RETURN HIGHER THAN 256 BECAUSE THE RETURN IS ACTUALLY A STATUS
+# HOWEVER: YOU CAN JUST DO THE MATH IN ECHO
+
+echo $(($myMut * $myImmut))
