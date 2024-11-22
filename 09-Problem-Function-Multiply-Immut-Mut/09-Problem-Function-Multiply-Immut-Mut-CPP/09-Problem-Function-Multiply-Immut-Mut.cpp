26e553cdb5bcfcedf45c605d3c88f33dbc1d7a85
Scott Feichter <scottfeichter@gmail.com>
2024-11-22 00:53:18 -0500

    misc (msg via sh alias)

--- 09-Problem-Function-Multiply-Immut-Mut/09-Problem-Function-Multiply-Immut-Mut-CPP/09-Problem-Function-Multiply-Immut-Mut.cpp
+++ 09-Problem-Function-Multiply-Immut-Mut/09-Problem-Function-Multiply-Immut-Mut-CPP/09-Problem-Function-Multiply-Immut-Mut.cpp
@@ -1,1 +1,18 @@
 // create a mutable variable and an immutable variable. create a function multiply that returns the product of 2 arguments. print the values of the variables and print their product using the multiply function.
+
+#include <iostream>
+using namespace std;
+
+int multiply(int num1, int num2) {
+    return num1 * num2;
+}
+
+int main() {
+
+    int myMut = 7;
+    const int MY_IMMUT = 9;
+
+    cout << "myMut: " << myMut << " MY_IMMUT: " << MY_IMMUT << " multiply: " << multiply(myMut, MY_IMMUT) << endl;
+
+    return 0;
+}
