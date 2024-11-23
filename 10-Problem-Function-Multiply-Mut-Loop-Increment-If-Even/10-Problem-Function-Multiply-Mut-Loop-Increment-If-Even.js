152582d3ef05a560d4eb120047cc17a0ef969003
Scott Feichter <scottfeichter@gmail.com>
2024-11-23 16:55:04 -0500

    misc (msg via sh alias)

--- 10-Problem-Function-Multiply-Mut-Loop-Increment-If-Even/10-Problem-Function-Multiply-Mut-Loop-Increment-If-Even.js
+++ 10-Problem-Function-Multiply-Mut-Loop-Increment-If-Even/10-Problem-Function-Multiply-Mut-Loop-Increment-If-Even.js
@@ -1,1 +1,21 @@
 // Create a mutable and an immutable. Create a function that will increment the mutable in the amount of the immutable  using a loop if the mutable is even. If the immutable is odd return the mutable
+
+
+let myMut = 4;
+const MY_IMMUT = 5;
+
+const isEven = (num) => {
+  return (num % 2 === 0);
+}
+
+const evenIncrement = (myMut, MY_IMMUT) => {
+  if (isEven(myMut)) {
+    for(let i = 0; i < MY_IMMUT; i++) {
+      myMut++
+    }
+    return myMut;
+  }
+  return myMut;
+}
+
+console.log(`myMut: ${myMut} MY_IMMUT: ${MY_IMMUT} evenIncrement: ${evenIncrement(myMut, MY_IMMUT)}`);
