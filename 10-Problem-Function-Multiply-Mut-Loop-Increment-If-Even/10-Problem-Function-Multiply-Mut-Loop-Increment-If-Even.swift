a15f3422e7006c2dabee9c9e0365877b7592e4dd
Scott Feichter <scottfeichter@gmail.com>
2024-11-23 12:08:07 -0500

    misc (msg via sh alias)

---
+++ 10-Problem-Function-Multiply-Mut-Loop-Increment-If-Even/10-Problem-Function-Multiply-Mut-Loop-Increment-If-Even.swift
@@ -0,0 +1,1 @@
+// Create a mutable and an immutable. Create a function that will increment the mutable in the amount of the immutable  using a loop if the mutable is even. If the immutable is odd return the mutable
