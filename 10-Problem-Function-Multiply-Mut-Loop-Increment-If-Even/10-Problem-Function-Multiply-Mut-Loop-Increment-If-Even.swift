52e24dddbc739fb7642136718afb08b40492f9d8
Scott Feichter <scottfeichter@gmail.com>
2024-11-23 23:36:47 -0500

    misc (msg via sh alias)

--- 10-Problem-Function-Multiply-Mut-Loop-Increment-If-Even/10-Problem-Function-Multiply-Mut-Loop-Increment-If-Even.swift
+++ 10-Problem-Function-Multiply-Mut-Loop-Increment-If-Even/10-Problem-Function-Multiply-Mut-Loop-Increment-If-Even.swift
@@ -1,1 +1,25 @@
 // Create a mutable and an immutable. Create a function that will increment the mutable in the amount of the immutable  using a loop if the mutable is even. If the immutable is odd return the mutable
+
+var myMut: Int = 4;
+let MY_IMMUT: Int = 5;
+
+func isEven(num: Int) -> Bool{
+    if(num % 2 == 0) {
+        return true;
+    }
+    return false;
+}
+
+func evenIncrement(num1: Int, num2: Int) -> Int {
+    if (isEven(num: num1)) {
+        let count: Int = num2;
+        while count > 0 {
+            num1 += 1;
+            num2 -= 1;
+        }
+        return num1;
+    }
+    return num1;
+}
+
+print("myMut: \(myMut) MY_IMMUT: \(MY_IMMUT) evenIncrement: \(evenIncrement(num1: myMut, num2: MY_IMMUT))");
