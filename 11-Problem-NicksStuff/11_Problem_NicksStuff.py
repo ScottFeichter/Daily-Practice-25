067d06373d0f4c533d55b22d0898d644272d70d2
Scott Feichter <scottfeichter@gmail.com>
2024-11-29 22:29:11 -0500

    misc (msg via sh alias)

--- 11-Problem-NicksStuff/11_Problem_NicksStuff.py
+++ 11-Problem-NicksStuff/11_Problem_NicksStuff.py
@@ -1,1 +1,22 @@
 # Nick makes variables in many languages
+
+harger = "unclescotty"
+print(harger)
+unclescotty = "get out of san fran"
+print(unclescotty)
+something = "mom please"
+print(something)
+mom = "let me use her hotspot at 2:20-3:30"
+print(mom)
+
+def nick_is_awesome(item):
+    print(f"Nick's {item} is awesome!!!")
+
+
+nick_is_awesome("uncleScotty")
+nick_is_awesome("yay")
+
+def caveman_is(cave):
+    print(f"caveman {cave} is")
+
+caveman_is("dad")
