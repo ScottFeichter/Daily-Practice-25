87718af44a6fc89a670a8084de1b0df9ae43ea39
Scott Feichter <scottfeichter@gmail.com>
2024-12-02 17:05:33 -0500

    misc (msg via sh alias)

--- 11-Problem-NicksStuff/11-Problem-NicksStuff.swift
+++ 11-Problem-NicksStuff/11-Problem-NicksStuff.swift
@@ -1,1 +1,20 @@
 // Nick makes variables in many languages
+
+var something: String = "mom please";
+print(something);
+
+var mom: String = "let me use her hotspot at 2:20-3:30";
+print(mom);
+
+func nickIsAwesome(item: String){
+    print("Nick's, \(item) is awesome!!!");
+}
+
+nickIsAwesome(item: "uncleScotty");
+nickIsAwesome(item: "yay");
+
+func cavemanis(cave: String){
+    print("caveman \(cave) is");
+}
+
+cavemanis(cave: "dad");
