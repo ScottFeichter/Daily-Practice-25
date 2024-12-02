e78ecd1ca255012b8d75c39466a6f67dd174501c
Scott Feichter <scottfeichter@gmail.com>
2024-12-02 17:24:13 -0500

    misc (msg via sh alias)

--- 11-Problem-NicksStuff/11-Problem-NicksStuff-CPP/11-Problem-NicksStuff.cpp
+++ 11-Problem-NicksStuff/11-Problem-NicksStuff-CPP/11-Problem-NicksStuff.cpp
@@ -1,1 +1,28 @@
 // Nick makes variables in many languages
+
+#include <iostream>
+using namespace std;
+
+void nickIsAwesome(string item){
+    cout << "Nick's " << item << " is awesome!!" << endl;
+}
+
+void cavemanis(string cave){
+    cout << "caveman " << cave << " is" << endl;
+}
+
+int main() {
+
+    string something = "mom please";
+    cout << something << endl;
+
+    string mom = "let me use her hotspot at 2:20-3:30";
+    cout << mom << endl;
+
+    nickIsAwesome("uncleScotty");
+    nickIsAwesome("yay");
+
+    cavemanis("dad");
+
+    return 0;
+}
--- 11-Problem-NicksStuff/11-Problem-NicksStuff.zsh
+++ 11-Problem-NicksStuff/11-Problem-NicksStuff.zsh
@@ -1,1 +1,23 @@
 # Nick makes variables in many languages
+
+something="mom please"
+echo $something
+
+mom="let me use her hotspot at 2:20-3:30"
+echo $mom
+
+function nickIsAwesome(){
+    # can't return string in shell script
+    echo "Nick's $1 is awsome!!!"
+
+}
+
+nickIsAwesome "uncleScotty"
+
+nickIsAwesome "yay"
+
+function cavemanis() {
+    echo "caveman $1 is"
+}
+
+cavemanis "dad"
