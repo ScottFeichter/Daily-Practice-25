c119918ac6df18e5391a5625526454c7c08c351d
Scott Feichter <scottfeichter@gmail.com>
2024-12-02 22:14:29 -0500

    misc (msg via sh alias)

---
+++ 11-Problem-NicksStuff/11-Problem-NicksStuff-TS/11-Problem-NicksStuff.js
@@ -0,0 +1,17 @@
+// Nick makes variables in many languages
+var hargar = "unclescotty";
+console.log(hargar);
+var unclescotty = "get out of san fran";
+console.log(unclescotty);
+var something = "mom please";
+console.log(something);
+var mom = "let me use her hotspot at 2:20-3:30";
+console.log(mom);
+function nickIsAwesome(item) {
+    console.log("Nick's ", item, " is awesome!!!");
+}
+nickIsAwesome("unclescotty");
+nickIsAwesome("yay");
+function cavemanis(cave) {
+    console.log("caveman ", cave, " is");
+}
--- 11-Problem-NicksStuff/11-Problem-NicksStuff-TS/11-Problem-NicksStuff.ts
+++ 11-Problem-NicksStuff/11-Problem-NicksStuff-TS/11-Problem-NicksStuff.ts
@@ -1,1 +1,22 @@
 // Nick makes variables in many languages
+
+
+let hargar: string = "unclescotty";
+console.log(hargar);
+let unclescotty = "get out of san fran";
+console.log(unclescotty);
+let something = "mom please";
+console.log(something);
+let mom = "let me use her hotspot at 2:20-3:30";
+console.log(mom);
+
+function nickIsAwesome(item: string) {
+  console.log("Nick's ", item, " is awesome!!!");
+}
+
+nickIsAwesome("unclescotty");
+nickIsAwesome("yay");
+
+function cavemanis(cave: string) {
+  console.log("caveman ", cave, " is");
+}
