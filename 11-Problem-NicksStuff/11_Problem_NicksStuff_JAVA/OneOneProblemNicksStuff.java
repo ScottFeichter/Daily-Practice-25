ff19334342e8b7c150cc5f3d9bb709571a95520c
Scott Feichter <scottfeichter@gmail.com>
2024-12-02 22:09:03 -0500

    misc (msg via sh alias)

--- 11-Problem-NicksStuff/11_Problem_NicksStuff_JAVA/OneOneProblemNicksStuff.java
+++ 11-Problem-NicksStuff/11_Problem_NicksStuff_JAVA/OneOneProblemNicksStuff.java
@@ -1,1 +1,27 @@
 // Nick makes variables in many languages
+ public class OneOneProblemNicksStuff {
+
+    static void nickIsAwesome(String item){
+        System.out.println("Nick's " + item + " is awesome!!");
+    }
+
+    static String cavemanis(String cave){
+        return "caveman " + cave + " is";
+    }
+
+    public static void main(String[] args){
+
+    String something = "mom please";
+    System.out.println(something);
+
+    String mom = "let me use her hotspot at 2:30-3:30";
+    System.out.println(mom);
+
+    nickIsAwesome("uncleScotty");
+    nickIsAwesome("yay");
+
+    cavemanis("dad");
+
+
+    }
+ }
