9c8619bcecb4789cb730d51a273b96b76545c1bb
Scott Feichter <scottfeichter@gmail.com>
2024-12-03 21:11:03 -0500

    misc (msg via sh alias)

--- 10-Problem-Function-Multiply-Mut-Loop-Increment-If-Even/one_zero_problem_function-multiply-mut-loop-increment-if-even_rust/src/main.rs
+++ 10-Problem-Function-Multiply-Mut-Loop-Increment-If-Even/one_zero_problem_function-multiply-mut-loop-increment-if-even_rust/src/main.rs
@@ -1,4 +1,35 @@
-fn main() {
-    println!("Hello, world!");
-}
 // Create a mutable and an immutable. Create a function that will increment the mutable in the amount of the immutable  using a loop if the mutable is even. If the immutable is odd return the mutable
+
+
+
+fn main() {
+
+    let mut my_mutable: u32 = 4;
+    let my_immutable: u32 = 5;
+
+    fn is_even(num: u32) -> bool {
+
+        if num % 2 == 0 {
+            return true;
+        }
+        return false;
+    }
+
+    fn even_increment(mut num1: u32, mut num2: u32) -> u32 {
+
+
+        if is_even(num1){
+            while num2 > 0 {
+                num1 += 1;
+                num2 -= 1;
+            }
+            return num1;
+        }
+        return num1;
+    }
+
+
+
+
+    println!("my_mutable: {my_mutable} my_immutable: {my_immutable} evenIncrement: {res}", res = even_increment(my_mutable, my_immutable));
+}
---
+++ 10-Problem-Function-Multiply-Mut-Loop-Increment-If-Even/one_zero_problem_function-multiply-mut-loop-increment-if-even_rust/target/debug/deps/one_zero_problem_function_multiply_mut_loop_increment_if_even_rust-a4d3eace0b6c7d4b.d
@@ -0,0 +1,5 @@
+/Users/scottfeichter/Dropbox/____DEV 2025/Daily-Practice-25/10-Problem-Function-Multiply-Mut-Loop-Increment-If-Even/one_zero_problem_function-multiply-mut-loop-increment-if-even_rust/target/debug/deps/one_zero_problem_function_multiply_mut_loop_increment_if_even_rust-a4d3eace0b6c7d4b: src/main.rs
+
+/Users/scottfeichter/Dropbox/____DEV 2025/Daily-Practice-25/10-Problem-Function-Multiply-Mut-Loop-Increment-If-Even/one_zero_problem_function-multiply-mut-loop-increment-if-even_rust/target/debug/deps/one_zero_problem_function_multiply_mut_loop_increment_if_even_rust-a4d3eace0b6c7d4b.d: src/main.rs
+
+src/main.rs:
