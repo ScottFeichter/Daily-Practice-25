6b9edfbdeb2e0d543b9c1dd063f0ffadc122c42b
Scott Feichter <scottfeichter@gmail.com>
2024-12-09 18:11:25 -0500

    misc (msg via sh alias)

--- 13-Problem-Function-Multiply-Mut-Loop-Immut/13-Problem-Function-Multiply-Mut-Loop-Immut.zsh
+++ 13-Problem-Function-Multiply-Mut-Loop-Immut/13-Problem-Function-Multiply-Mut-Loop-Immut.zsh
@@ -1,1 +1,22 @@
 # Create a mutable variable identified as num1 and an immutable variable as num2. Create a function to check if num1 is even - if yes increment num1 by the amount of num2. If no then return num1.
+
+
+num1=4
+readonly num2=7
+
+function isEvenIncrement(){
+    r=$(($num1 % 2))
+    if [[ $r -eq 0 ]] # THE SPACES HERE ARE NEEDED OTHERWISE IT WON'T WORK...SHELL SCRIPT IS WEIRD
+
+    then i=$num2
+        while $i > 0;
+        do $num1++ $i--
+        done
+        echo $num1
+    fi
+    echo $num1
+}
+
+echo "num1: " $num1 " num2: " $num2 " isEvenIncrement: ";
+
+isEvenIncrement
