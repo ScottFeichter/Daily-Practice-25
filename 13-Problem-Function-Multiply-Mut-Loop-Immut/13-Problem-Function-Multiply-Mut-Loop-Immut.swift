086654dfe2a2b24f0760c7a307bda45fd288ccc0
Scott Feichter <scottfeichter@gmail.com>
2024-12-09 11:32:54 -0500

    misc (msg via sh alias)

--- 13-Problem-Function-Multiply-Mut-Loop-Immut/13-Problem-Function-Multiply-Mut-Loop-Immut.swift
+++ 13-Problem-Function-Multiply-Mut-Loop-Immut/13-Problem-Function-Multiply-Mut-Loop-Immut.swift
@@ -1,1 +1,19 @@
 // Create a mutable variable identified as num1 and an immutable variable as num2. Create a function to check if num1 is even - if yes increment num1 by the amount of num2. If no then return num1.
+
+
+var num1: Int = 4;
+let num2: Int = 7;
+
+func isEvenIncrement() -> Int {
+    if(num1 % 2 == 0) {
+        var i: Int = num2;
+        while i > 0 {
+            num1+=1;
+            i-=1;
+        }
+        return num1;
+    }
+    return num2;
+}
+
+print("num1: \(num1) num2: \(num2) isEvenIncrement: \(isEvenIncrement())");
