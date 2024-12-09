405619939ff9279f3b47a2583b4762eaccd984ff
Scott Feichter <scottfeichter@gmail.com>
2024-12-09 11:36:46 -0500

    misc (msg via sh alias)

--- 13-Problem-Function-Multiply-Mut-Loop-Immut/13_Problem_Function-Multiply-Mut-Loop-Immut.py
+++ 13-Problem-Function-Multiply-Mut-Loop-Immut/13_Problem_Function-Multiply-Mut-Loop-Immut.py
@@ -1,1 +1,17 @@
 # Create a mutable variable identified as num1 and an immutable variable as num2. Create a function to check if num1 is even - if yes increment num1 by the amount of num2. If no then return num1.
+
+
+num1 = 4
+NUM_2 = 7
+
+def isEvenIncrement():
+    global num1
+    if num1 % 2 == 0:
+        i = NUM_2
+        while i > 0:
+            num1+=1
+            i-= 1
+        return num1
+    return num1
+
+print(f"num1: {num1} NUM_2: {NUM_2} isEvenIncrement: {isEvenIncrement()}")
