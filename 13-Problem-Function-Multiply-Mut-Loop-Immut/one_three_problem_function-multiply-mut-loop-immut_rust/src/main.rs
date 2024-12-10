ac126a13647d7d4a79c2dc43e7893c7f3c48163e
Scott Feichter <scottfeichter@gmail.com>
2024-12-09 21:21:33 -0500

    misc (msg via sh alias)

--- 13-Problem-Function-Multiply-Mut-Loop-Immut/one_three_problem_function-multiply-mut-loop-immut_rust/src/main.rs
+++ 13-Problem-Function-Multiply-Mut-Loop-Immut/one_three_problem_function-multiply-mut-loop-immut_rust/src/main.rs
@@ -1,4 +1,25 @@
-fn main() {
-    println!("Hello, world!");
-}
 // Create a mutable variable identified as num1 and an immutable variable as num2. Create a function to check if num1 is even - if yes increment num1 by the amount of num2. If no then return num1.
+
+// scoping problem...need side quest
+
+
+fn main() {
+
+    static mut num1: u32 = 4;
+    static num2: u32 = 7;
+
+    fn is_even_increment() -> u32 {
+
+        if num1 % 2 == 0 {
+            let mut i: u32 = num2;
+            while i > 0 {
+                num1+=1;
+                i-=1;
+            }
+            return num1;
+        }
+        return num1;
+    }
+
+    println!("{}", is_even_increment());
+}
---
+++ 13-Problem-Function-Multiply-Mut-Loop-Immut/one_three_problem_function-multiply-mut-loop-immut_rust/target/debug/deps/one_three_problem_function_multiply_mut_loop_immut_rust-c209f1af03fbf412.d
@@ -0,0 +1,5 @@
+/Users/scottfeichter/Dropbox/____DEV 2025/Daily-Practice-25/13-Problem-Function-Multiply-Mut-Loop-Immut/one_three_problem_function-multiply-mut-loop-immut_rust/target/debug/deps/one_three_problem_function_multiply_mut_loop_immut_rust-c209f1af03fbf412: src/main.rs
+
+/Users/scottfeichter/Dropbox/____DEV 2025/Daily-Practice-25/13-Problem-Function-Multiply-Mut-Loop-Immut/one_three_problem_function-multiply-mut-loop-immut_rust/target/debug/deps/one_three_problem_function_multiply_mut_loop_immut_rust-c209f1af03fbf412.d: src/main.rs
+
+src/main.rs:
