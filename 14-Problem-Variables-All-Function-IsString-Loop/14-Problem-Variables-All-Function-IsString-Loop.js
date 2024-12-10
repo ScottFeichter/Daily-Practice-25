83e33b0b1cbcba9e4009685a70954ff0086f8793
Scott Feichter <scottfeichter@gmail.com>
2024-12-10 14:41:12 -0500

    misc (msg via sh alias)

---
+++ 14-Problem-Variables-All-Function-IsString-Loop/14-Problem-Variables-All-Function-IsString-Loop.js
@@ -0,0 +1,32 @@
+// Create an immut var id num, immut var id str1, str2, str3, immut var id bools, immut var id char, immut var floats. Create an mut var id count. Create a fn id isString to return true false if arg is a string. Create a fn with no return that increments count if the arg is a string. Print all the vars and print count before and after checking all the vars.
+
+
+const num = 3;
+const str1 = "str1";
+const str2 = "str2";
+const str3 = "str3";
+const bools = false;
+const char = "c";
+const floats = 32.32;
+
+let count = 0;
+
+const isString = (primitive) => {
+  return (typeof primitive === "string");
+}
+
+const incrementCount = (variable) => {
+  if (isString(variable)) count++;
+}
+
+console.log(num, str1, str2, str3, bools, char, floats, count);
+
+incrementCount(num);
+incrementCount(str1);
+incrementCount(str2);
+incrementCount(str3);
+incrementCount(bools);
+incrementCount(char);
+incrementCount(floats);
+
+console.log(`final count: ${count}`);
---
+++ 14-Problem-Variables-All-Function-IsString-Loop/one_four_problem_variables-all-function-isstring-loop_rust/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("Hello, world!");
+}
+// Create an immut var id num, immut var id str1, str2, str3, immut var id bools, immut var id char, immut var floats. Create an mut var id count. Create a fn id isString to return true false if arg is a string. Create a fn with no return that increments count if the arg is a string. Print all the vars and print count before and after checking all the vars.
