54f005c7b030f4364589a65952c08de5aad9d123
Scott Feichter <scottfeichter@gmail.com>
2024-12-11 19:58:38 -0500

    misc (msg via sh alias)

--- 14-Problem-Variables-All-Function-IsString-Loop/one_four_problem_variables-all-function-isstring-loop_rust/src/main.rs
+++ 14-Problem-Variables-All-Function-IsString-Loop/one_four_problem_variables-all-function-isstring-loop_rust/src/main.rs
@@ -1,4 +1,19 @@
-fn main() {
-    println!("Hello, world!");
-}
 // Create an immut var id num, immut var id str1, str2, str3, immut var id bools, immut var id char, immut var floats. Create an mut var id count. Create a fn id isString to return true false if arg is a string. Create a fn with no return that increments count if the arg is a string. Print all the vars and print count before and after checking all the vars.
+
+// problem: again how to make a function that takes an arg of unknown type
+
+fn main() {
+
+    let num: u32 = 3;
+    let str1: &str = "str1";
+    let str2: &str = "str2";
+    let str3: &str = "str3";
+    let bools: bool = false;
+    let chars: char = 'c';
+
+    let mut count: u32 = 0;
+
+
+
+    println!("{}", num.type_of());
+}
---
+++ 14-Problem-Variables-All-Function-IsString-Loop/one_four_problem_variables-all-function-isstring-loop_rust/target/debug/deps/one_four_problem_variables_all_function_isstring_loop_rust-a5ba76dd087713e9.d
@@ -0,0 +1,5 @@
+/Users/scottfeichter/Dropbox/____DEV 2025/Daily-Practice-25/14-Problem-Variables-All-Function-IsString-Loop/one_four_problem_variables-all-function-isstring-loop_rust/target/debug/deps/one_four_problem_variables_all_function_isstring_loop_rust-a5ba76dd087713e9: src/main.rs
+
+/Users/scottfeichter/Dropbox/____DEV 2025/Daily-Practice-25/14-Problem-Variables-All-Function-IsString-Loop/one_four_problem_variables-all-function-isstring-loop_rust/target/debug/deps/one_four_problem_variables_all_function_isstring_loop_rust-a5ba76dd087713e9.d: src/main.rs
+
+src/main.rs:
