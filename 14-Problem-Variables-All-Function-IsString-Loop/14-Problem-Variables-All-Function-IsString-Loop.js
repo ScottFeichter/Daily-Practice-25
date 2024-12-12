4e05356e05310735740cb8782e2f619c2102536c
Scott Feichter <scottfeichter@gmail.com>
2024-12-11 20:21:21 -0500

    misc (msg via sh alias)

--- 14-Problem-Variables-All-Function-IsString-Loop/14-Problem-Variables-All-Function-IsString-Loop.js
+++ 14-Problem-Variables-All-Function-IsString-Loop/14-Problem-Variables-All-Function-IsString-Loop.js
@@ -1,6 +1,5 @@
 // Create an immut var id num, immut var id str1, str2, str3, immut var id bools, immut var id char, immut var floats. Create an mut var id count. Create a fn id isString to return true false if arg is a string. Create a fn with no return that increments count if the arg is a string. Print all the vars and print count before and after checking all the vars.
 
-
 const num = 3;
 const str1 = "str1";
 const str2 = "str2";
