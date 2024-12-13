975d5fc53ab23ef3f78b7e52a944ca4877397103
Scott Feichter <scottfeichter@gmail.com>
2024-12-13 17:37:34 -0500

    misc (msg via sh alias)

--- 16-Problem-RandomFifteen/16_Problem_RandomFifteen.py
+++ 16-Problem-RandomFifteen/16_Problem_RandomFifteen.py
@@ -1,1 +1,26 @@
 # Do everything in problem 15 but make the int generated from randome between 1 - 10
+import random
+
+nint = random.randint(1, 10)
+flote = 32.32
+chr = 'c'
+str = "hello"
+boo = True
+
+def incrementNint(num):
+    if nint > 5:
+        adder = random.randint(1,10)
+        loops = random.randint(1, 10)
+
+        print(f"adder: {adder}")
+        print(f"loops: {loops}")
+
+        while loops > 0:
+            num+= adder
+            adder+= 1
+            loops-= 1
+
+    return num
+
+print(f"nint: {nint} flote: {flote} chr: {chr} str: {str} boo: {boo}")
+print(incrementNint(nint))
