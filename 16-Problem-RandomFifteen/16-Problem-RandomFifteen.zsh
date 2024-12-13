b4d5efd90604dac65d6e307cc204588566b05363
Scott Feichter <scottfeichter@gmail.com>
2024-12-13 09:45:48 -0500

    misc (msg via sh alias)

---
+++ 16-Problem-RandomFifteen/16-Problem-RandomFifteen.zsh
@@ -0,0 +1,1 @@
+# Do everything in problem 15 but make the int generated from randome between 1 - 10
---
+++ 16-Problem-RandomFifteen/one_six_problem_randomfifteen_rust/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("Hello, world!");
+}
+// Do everything in problem 15 but make the int generated from randome between 1 - 10
