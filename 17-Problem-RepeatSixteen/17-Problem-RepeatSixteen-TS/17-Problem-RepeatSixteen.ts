e566931422b8abb9a8a2399f42696e94f0a73a14
Scott Feichter <scottfeichter@gmail.com>
2024-12-24 11:16:00 -0500

    misc (msg via sh alias)

--- 17-Problem-RepeatSixteen/17-Problem-RepeatSixteen-TS/17-Problem-RepeatSixteen.ts
+++ 17-Problem-RepeatSixteen/17-Problem-RepeatSixteen-TS/17-Problem-RepeatSixteen.ts
@@ -1,1 +1,12 @@
 // Repeat problem 16 to work in the rote memorization of declaring most types, generating random numbers in a range, declaring a functions, using loops, using conditionals, and printing to console.
+
+
+const nint: Number = Math.floor(Math.random() * (10 -1)) + 1;
+
+
+
+
+
+
+
+let adder: Number = Math.floor(Math.random() * (10 - 1)) + 1;
