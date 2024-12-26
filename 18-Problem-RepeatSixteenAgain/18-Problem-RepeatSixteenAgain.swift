2c0928e5ca7c8ec09fccd0ade1e69c04fd0496fd
Scott Feichter <scottfeichter@gmail.com>
2024-12-26 15:28:20 -0500

    misc (msg via sh alias)

--- 18-Problem-RepeatSixteenAgain/18-Problem-RepeatSixteenAgain.swift
+++ 18-Problem-RepeatSixteenAgain/18-Problem-RepeatSixteenAgain.swift
@@ -1,1 +1,27 @@
 // Repeat problem 16 again to work rote memory of basic syntax.
+
+let NINT: Int = Int.random(in: 1..<10);
+let FLOTE: Float = 32.32;
+let CHR: Character = "c";
+let STR: String = "String";
+let BOO: Bool = true;
+
+func incrementNint(num: Int) -> Int {
+    var local_num: Int = num;
+    if num > 5 {
+        var adder: Int = Int.random(in: 1..<10);
+        var loops: Int = Int.random(in: 1..<10);
+
+        while loops > 0 {
+            local_num+=adder;
+            adder+=1;
+            loops-=1;
+
+        }
+    }
+  return local_num;
+}
+
+print("NINT: \(NINT) FLOTE: \(FLOTE) CHR: \(CHR) STR: \(STR) BOO: \(BOO)");
+
+print("incrementNint(NINT): \(incrementNint(num: NINT))");
--- 18-Problem-RepeatSixteenAgain/18-Problem-RepeatSixteenAgain.zsh
+++ 18-Problem-RepeatSixteenAgain/18-Problem-RepeatSixteenAgain.zsh
@@ -1,1 +1,27 @@
 # Repeat problem 16 again to work rote memory of basic syntax.
+
+
+declare -i -r NINT=$(($(($RANDOM%$((10-1+1))))+X));
+declare -i -r NOTFLOTE=32;
+readonly CHR='c';
+readonly STR="String";
+readonly BOO=1;
+
+function incrementNint() {
+    local -i local_num=$1;
+    if (($local_num > 5)); then
+        local -i adder=$(($(($RANDOM%$((10-1+1))))+X));
+        local -i loops=$(($(($RANDOM%$((10-1+1))))+X));
+
+        while (($loops > 0)); do
+            local_num=$(($local_num + $adder));
+            adder=$(($adder + 1));
+            loops=$(($loops - 1));
+        done
+    fi
+    echo $local_num;
+}
+
+echo "NINT: " $NINT " NOTFLOTE: " $NOTFLOTE " CHR: " $CHR " STR: " $STR " BOO: " $BOO
+
+incrementNint $NINT
