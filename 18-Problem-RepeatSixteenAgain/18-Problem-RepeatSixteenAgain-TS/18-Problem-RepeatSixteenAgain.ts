120772ed0df5495e8a17c6582e0f875a8d2e6fc2
Scott Feichter <scottfeichter@gmail.com>
2024-12-26 23:42:12 -0500

    misc (msg via sh alias)

--- 18-Problem-RepeatSixteenAgain/18-Problem-RepeatSixteenAgain-TS/18-Problem-RepeatSixteenAgain.ts
+++ 18-Problem-RepeatSixteenAgain/18-Problem-RepeatSixteenAgain-TS/18-Problem-RepeatSixteenAgain.ts
@@ -1,1 +1,22 @@
 // Repeat problem 16 again to work rote memory of basic syntax.
+
+const NINT: number = Math.floor(Math.random() * (10 -1) + 1);
+const FLOTE: number = 32.32;
+const CHR: string = 'c';
+const STR: string = "String";
+const BOO: boolean = true;
+
+const incrementNint = (num: number) => {
+  if(num > 5) {
+    let adder: number = Math.floor(Math.random() * (10 - 1) + 1);
+    let loops: number = Math.floor(Math.random() * (10 - 1) + 1);
+
+    while(loops > 0) {
+      num+= adder;
+      adder++;
+      loops--;
+    }
+  }
+  return num;
+}
+
