da4924cc1a27b15c9a6d3d176ff8cd9d035756a3
Scott Feichter <scottfeichter@gmail.com>
2025-01-01 00:01:26 -0500

    misc (msg via sh alias)

--- 19-Problem-RepeatSixteenAndDefineArray/19-Problem-RepeatSixteenAndDefineArray.zsh
+++ 19-Problem-RepeatSixteenAndDefineArray/19-Problem-RepeatSixteenAndDefineArray.zsh
@@ -1,1 +1,29 @@
 # Repeat problem Sixten but this time also create an array of nums, an array of chars, an array of strings, and an array of booleans
+
+declare -i -r NINT=$(($(($RANDOM%$((10-1+1))))+X));
+declare -i -r NOTFLOTE=32;
+readonly CHR='c';
+readonly STR="String";
+readonly BOO=1;
+
+nums=(0 1 2 3);
+notflotes=(0 1 2 3);
+chars=(a b c d);
+stirs=(Hello World I am Scott);
+boos=(true false false true);
+
+
+function incrementNint() {
+    local -i local_num=$1;
+    if (($local_num > 5)); then
+        local -i adder=$(($(($RANDOM%$((10-1+1))))+X));
+        local -i loops=$(($(($RANDOM%$((10-1+1))))+X));
+
+        while (($loops > 0)); do
+            local_num=$(($local_num + $adder));
+            adder=$(($adder + 1));
+            loops=$(($loops - 1));
+        done
+    fi
+    echo $local_num;
+}
--- 19-Problem-RepeatSixteenAndDefineArray/19_Problem_RepeatSixteenAndDefineArray_JAVA/OneNineProblemRepeatSixteenAndDefineArray.java
+++ 19-Problem-RepeatSixteenAndDefineArray/19_Problem_RepeatSixteenAndDefineArray_JAVA/OneNineProblemRepeatSixteenAndDefineArray.java
@@ -1,1 +1,44 @@
 // Repeat problem Sixten but this time also create an array of nums, an array of chars, an array of strings, and an array of booleans
+
+import java.io.*;
+import java.util.*;
+
+public class OneNineProblemRepeatSixteenAndDefineArray {
+
+    public static int incrementNint(int num) {
+        if(num > 5) {
+            int adder = (1 + (int)(Math.random() * ((10 - 1) + 1)));
+            int loops = (1 + (int)(Math.random() * ((10 - 1) + 1)));
+
+            while(loops > 0) {
+                num+= adder;
+                adder+=1;
+                loops-=1;
+            }
+        }
+        return num;
+    }
+
+
+    public static void main(String[] args) {
+
+        final int NINT = (1 + (int)(Math.random() * ((10 - 1) + 1)));
+        final double DBL = 32.32;
+        final char CHR = 'c';
+        final String STR = "String";
+        final boolean BOO = true;
+
+        int[] nums = {0, 1, 2, 3};
+        double[] dubs = {11.11, 22.22, 33.33, 44.44};
+        char[] chars = {'a', 'b', 'c', 'd'};
+        String[] stirs = {"Hello", "World", "I", "am", "Scott"};
+        boolean[] boos = {true, false, false, true};
+
+        System.out.println("NINT: " + NINT + " DOUBLE: " + DBL + " CHR: " + CHR + "STR: " + STR + " BOO: " + BOO);
+
+        System.out.println("nums: " + nums + " dubs: " + dubs + " chars: " + chars + " stirs: " + stirs + " boos: " + boos);
+
+        System.out.println("incrementNint(NINT): " + incrementNint(NINT));
+
+    }
+}
