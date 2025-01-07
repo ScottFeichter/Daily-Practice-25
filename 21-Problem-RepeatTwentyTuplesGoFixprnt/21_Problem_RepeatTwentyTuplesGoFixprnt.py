cc68d02499d26148549a46c8865bf0c7e6b6cadf
Scott Feichter <scottfeichter@gmail.com>
2025-01-07 11:21:17 -0500

    misc (msg via sh alias)

---
+++ 21-Problem-RepeatTwentyTuplesGoFixprnt/21_Problem_RepeatTwentyTuplesGoFixprnt.py
@@ -0,0 +1,1 @@
+# Repeat problem 20 - make the mixed arrays tuples (they already are), fix the printing by reference, and add the Go language
---
+++ 21-Problem-RepeatTwentyTuplesGoFixprnt/two_one_problem_repeattwentytuplesgofixprnt_rust/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("Hello, world!");
+}
+// Repeat problem 20 - make the mixed arrays tuples (they already are), fix the printing by reference, and add the Go language
