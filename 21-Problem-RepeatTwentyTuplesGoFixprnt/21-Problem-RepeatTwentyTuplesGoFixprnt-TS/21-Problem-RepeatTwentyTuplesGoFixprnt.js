8fe8b1e6f18665c83679896f31c9cc91f37a81d9
Scott Feichter <scottfeichter@gmail.com>
2025-01-12 14:13:57 -0800

    misc (msg via sh alias)

---
+++ 21-Problem-RepeatTwentyTuplesGoFixprnt/21-Problem-RepeatTwentyTuplesGoFixprnt-TS/21-Problem-RepeatTwentyTuplesGoFixprnt.js
@@ -0,0 +1,29 @@
+// Repeat problem 20 - make the mixed arrays tuples (they already are), fix the printing by reference, and add the Go language
+var NINT = Math.floor(Math.random() * (10 - 1) + 1);
+var FLOTE = 32.32;
+var CHR = 'c';
+var STR = "string";
+var BOO = false;
+var undv;
+var nums = [0, 1, 2, 3];
+var flotes = [11.11, 22.22, 33.33, 44.44];
+var chars = ['a', 'b', 'c', 'd'];
+var stirs = ["Hello", "World", "I", "am", "Scott"];
+var boos = [true, false, false, true];
+var mixta = [0, 'b', "three", false];
+var unda = [];
+var incrementNint = function (num) {
+    if (num > 5) {
+        var adder = Math.floor(Math.random() * (10 - 1) + 1);
+        var loops = Math.floor(Math.random() * (10 - 1) + 1);
+        while (loops > 0) {
+            num += adder;
+            adder++;
+            loops--;
+        }
+    }
+    return num;
+};
+console.log("NINT: ".concat(NINT, " FLOTE: ").concat(FLOTE, " CHR: ").concat(CHR, " STR: ").concat(STR, " BOO: ").concat(BOO, " undv: ").concat(undv));
+console.log("nums: ".concat(nums, " chars: ").concat(chars, " stirs: ").concat(stirs, " boos: ").concat(boos, " mixta: ").concat(mixta, " unda: ").concat(unda));
+console.log("incrementNint(NINT): ".concat(incrementNint(NINT)));
--- 21-Problem-RepeatTwentyTuplesGoFixprnt/21-Problem-RepeatTwentyTuplesGoFixprnt-TS/21-Problem-RepeatTwentyTuplesGoFixprnt.ts
+++ 21-Problem-RepeatTwentyTuplesGoFixprnt/21-Problem-RepeatTwentyTuplesGoFixprnt-TS/21-Problem-RepeatTwentyTuplesGoFixprnt.ts
@@ -1,1 +1,37 @@
 // Repeat problem 20 - make the mixed arrays tuples (they already are), fix the printing by reference, and add the Go language
+
+const NINT: number = Math.floor(Math.random() * (10 -1) + 1);
+const FLOTE: number = 32.32;
+const CHR: string = 'c';
+const STR: string = "string";
+const BOO: boolean = false;
+
+let undv: number;
+
+let nums: number[] = [0, 1, 2, 3];
+let flotes: number[] = [11.11, 22.22, 33.33, 44.44];
+let chars: string[] = ['a', 'b', 'c', 'd'];
+let stirs: string[] = ["Hello", "World", "I", "am", "Scott"];
+let boos: boolean[] = [true, false, false, true];
+
+let mixta = [0, 'b', "three", false];
+let unda: any[] = [];
+
+const incrementNint = (num: number) => {
+  if(num > 5){
+    let adder = Math.floor(Math.random() * (10 -1) + 1);
+    let loops = Math.floor(Math.random() * (10 -1) + 1);
+
+    while(loops > 0){
+      num+= adder;
+      adder++;
+      loops--;
+    }
+  }
+  return num
+}
+
+
+console.log(`NINT: ${NINT} FLOTE: ${FLOTE} CHR: ${CHR} STR: ${STR} BOO: ${BOO} undv: ${undv}`);
+console.log(`nums: ${nums} chars: ${chars} stirs: ${stirs} boos: ${boos} mixta: ${mixta} unda: ${unda}`);
+console.log(`incrementNint(NINT): ${incrementNint(NINT)}`);
