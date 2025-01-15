3c0048ec5316fccae1af04b322f6b66e6ac9cb03
Scott Feichter <scottfeichter@gmail.com>
2025-01-14 22:50:17 -0800

    misc (msg via sh alias)

--- 22-Problem-B75-Duplicate-Integer/22-Problem-B75-Duplicate-Integer.go
+++ 22-Problem-B75-Duplicate-Integer/22-Problem-B75-Duplicate-Integer.go
@@ -1,7 +1,50 @@
-package main // \?
+
+// arrays have fixed size in go - slices do not
+
+package main
 
 import (
 	"fmt"
-	"math/rand"
-
 )
+
+func containsDuplicate(nums []int) bool {
+	// should we be checking for array of mixed type?
+		var seenNums = make(map[int]bool);
+
+		for _, num := range nums {
+			if seenNums[num] {
+				return true
+			}
+			seenNums[num] = true
+		}
+
+
+		return false
+}
+
+
+	// no sets in go lang so rather than implement set in hash just skip it
+
+
+
+func main() {
+
+
+	var nums1 = []int{0, 1, 2, 3};
+	var nums2 = []int{1, 2, 3, 3} // one dup
+	var nums3 = []int{1, 1, 2, 2, 3} // two dups
+	var nums4 = []int{1, 2, 3, 3, 3} // a triple dup
+	var nums5 = []int{} // edge empty array
+	var nums6 = []int{1} // edge single element array
+	// const nums7 = [4]int{1, 'a', 3.3, "four!", 1} // not applicable because should be only nums
+
+
+	fmt.Println(containsDuplicate(nums1)) // false
+	fmt.Println(containsDuplicate(nums2)) // true
+	fmt.Println(containsDuplicate(nums3)) // true
+	fmt.Println(containsDuplicate(nums4)) // true
+	fmt.Println(containsDuplicate(nums5)) // false
+	fmt.Println(containsDuplicate(nums6)) // false
+	// console.log(containsDuplicate(nums7)) // true ***
+
+}
